36392357ff2b1ef0458f42eec450cbf41a3b2fca
msherman13 <miles.sherman@me.com>
2013-03-28 16:28:18 -0400

    L3 Add Lookup file compiled successfully. Not yet tested.

--- src/l3_add_lookup.vhd
+++ src/l3_add_lookup.vhd
@@ -19,8 +19,8 @@ entity l3_add_lookup is
 		reset			:	in	std_logic;
 		clk				:	in	std_logic;
 		add_enable		:	in	std_logic;
-		new_order		:	in	std_logic_vector (21 downto 0);	-- New order for comparison.
-		book_order		:	in	std_logic_vector (21 downto 0); -- Order under test.
+		new_order		:	in	std_logic_vector (175 downto 0);	-- New order for comparison.
+		book_order		:	in	std_logic_vector (175 downto 0); -- Order under test.
 		address_final	:	out std_logic;						-- Outgoing flag to indicate address value is final.
 		address			:	out	std_logic_vector (31 downto 0)	-- Outgoing address.
 	);
@@ -30,12 +30,12 @@ end l3_add_lookup;
 
 architecture rtl of l3_add_lookup is
 
-constant l3_offset		: integer := 0; -- L3 memory offset address.
+constant l3_offset		: signed (31 downto 0) := (others => '0'); -- L3 memory offset address.
 signal price_match		: std_logic;
 signal read_enable		: std_logic;
-signal calc_addr_final	: std_logic := 0;
-signal addr_count		: std_logic_vector (31 downto 0) := 0;
-signal new_order_addr	: std_logic_vector (31 downto 0) := 0;
+signal calc_addr_final	: std_logic;
+signal addr_count		: signed (31 downto 0);
+signal new_order_addr	: signed (31 downto 0);
 
 begin
 
@@ -46,32 +46,34 @@ begin
 
 	-- Initializations for the testing.
 	read_enable <= '1';
-	addr_count <= l3_offset;
-	price_match <= '0'
+	price_match <= '0';
 	calc_addr_final <= '0';
+	addr_count <= l3_offset;
+	new_order_addr <= l3_offset;
 
-	while (calc_addr_final = '0') loop	-- Begin iteration loop.
-		if (price_match = '0')												
-			if (unsigned(book_order(18 downto 13)) = unsigned(new_order(18 downto 13))) then	-- Does the stock symbol match?
-				if (unsigned(book_order(22 downto 19)) = unsigned(new_order(22 downto 19))) then	-- Does the price match?
+	if (calc_addr_final = '0') then	-- Begin iterations.
+		address <= std_logic_vector(addr_count);
+		if (price_match = '0') then									
+			if (unsigned(book_order(135 downto 95)) = unsigned(new_order(135 downto 95))) then-- Does stock match?
+				if (unsigned(book_order(175 downto 143)) = unsigned(new_order(175 downto 143))) then-- Does price match?
 					price_match <= '1';	-- Flag to move to next FSM.
-					addr_count++;	-- Increment the address to final value.
+					addr_count <= addr_count + 1;	-- Increment the address to final value.
 				else
-					addr_count++;	-- Continue iterative testing.
+					addr_count <= addr_count + 1;	-- Continue iterative testing.
 				end if;
 			else
-				addr_count++;	-- Continue iterative testing.
+				addr_count <= addr_count + 1;	-- Continue iterative testing.
 			end if;
 		else
-			switch
-			if (book_order(22 downto 19) != new_order(22 downto 19)) or (book_order(18 downto 13) != new_order(18 downto 13))) then	-- Has the count exceeded the price level or the stock symbol changed?
+			if ((book_order(175 downto 143) /= new_order(175 downto 143)) or (book_order(135 downto 95) /= new_order(135 downto 95))) then	-- Has the count exceeded the price level or the stock symbol changed?
 				new_order_addr <= addr_count;	-- Set address count to final addr value.
+				address <= std_logic_vector(new_order_addr);
 				calc_addr_final <= '1';	-- Signal that look up is complete.
 			else
-				addr_count++;	-- If prices still match, continue count.
+				addr_count <= addr_count + 1;	-- If prices still match, continue count.
 			end if;
 		end if;
-	end loop;
+	end if;
 end process;
 
 end rtl;
